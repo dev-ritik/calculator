d04bc89d4727a9872bc84229660c11a096fc31ba
dev-ritik <ritikkne@gmail.com>
2018-10-09 00:53:53 +0530

    minor changes @ verilog files

--- ALU.v
+++ ALU.v
@@ -1,9 +1,17 @@
+// GroupID-12 (17114063_17114059) - Ritik kumar & Prateek mali
+// GroupID-2 (17114049_17114037) - Harshit maurya & Mularam choudhary
+// Date: Oct 8, 2018
+// ALU.v - This is the central verilog file which receives the user input, and processes a task based upon the control (OPcode) received.
+//			  It also have a SevenSegmentEncoder task to convert the bcd output to the required 7-segment code.
+
+
+
 module ALU (i1,i2,ctrl,o);
-  input[3:0] i1,i2;
-  input[1:0] ctrl;
-  output[6:0] o;
-  reg[6:0] o;
-  reg[3:0] r;
+  input[3:0] i1,i2;  //input 1 & input 2
+  input[1:0] ctrl; 	//OP code
+  output[6:0] o; 		//output in seven segment code
+  reg[6:0] o;        //output reg
+  reg[3:0] r;			//temp reg holding output in bcd
   `include "Add.v"
 
   always @(i1 or i2 or ctrl or o)
--- ALU_tb.v
+++ ALU_tb.v
@@ -1,36 +1,21 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   23:17:58 10/07/2018
-// Design Name:   ALU
-// Module Name:   /media/ritik/Ritik/cse_work/verilog/calculator/ALU_tb.v
-// Project Name:  calculator
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: ALU
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+// GroupID-12 (17114063_17114059) - Ritik kumar & Prateek mali
+// GroupID-2 (17114049_17114037) - Harshit maurya & Mularam choudhary
+// Date: Oct 8, 2018
+// ALU_tb.v - This is a testbench file which sits on the top of ALU.v file and it receives the user input via an 'input.txt' file
+//						 and writes the corresponding output in to the 'output.txt' file.
+
 
 module ALU_tb;
 
 	// Inputs
-	reg [3:0] i1;
-	reg [3:0] i2;
-	reg [1:0] ctrl;
+	reg [3:0] i1; 	  //input 1
+	reg [3:0] i2;    //input 2
+	reg [1:0] ctrl;  //OP code
 
 	// Outputs
-	wire [6:0] o;
+	wire [6:0] o;    //output in seven segment code
 
 	// Instantiate the Unit Under Test (UUT)
 	ALU uut (
@@ -42,65 +27,34 @@ module ALU_tb;
 
 	reg [9:0] mem[1:0];
 	integer outputFile;
-	initial begin
-		
-		
-		// Initialize Inputs
-		/*i1 = 4'b0110;
-		i2 = 4'b0010;
-		ctrl = 2'b00;
-
-		// Wait 10 ns for global reset to finish
-		#10;	
-		
-		i1 = 4'b0110;
-		i2 = 4'b0010;
-		ctrl = 2'b01;
-
-		// Wait 10 ns for global reset to finish
-		#10;
-		i1 = 4'b0110;
-		i2 = 4'b0010;
-		ctrl = 2'b10;
-
-		// Wait 10 ns for global reset to finish
-		#10;
-		i1 = 4'b0110;
-		i2 = 4'b0010;
-		ctrl = 2'b11;
-
-		// Wait 10 ns for global reset to finish
-		#10;
-        
-		// Add stimulus here
-*/
-		// default values
-		i1 = 4'b1111;
-		i2 = 4'b1111;
-		ctrl = 2'b11;
-		#100;
-
-		//read values from file
-		$readmemb("input.txt",mem);
-		#10;
-
-		//assign
-		{ctrl,i1,i2}=mem[0];
-		
-		#100;
-		
-		outputFile = $fopen("output.txt","w");
-
-
+	initial
 		begin
+		
+			// default values
+			i1 = 4'b1111;
+			i2 = 4'b1111;
+			ctrl = 2'b11;
 			#100;
-			//write to external file
-			$fwrite(outputFile,"%b\n",o);
+
+			//read values from file
+			$readmemb("input.txt",mem);
+			#10;
+
+			//assign
+			{ctrl,i1,i2}=mem[0];
+			
+			outputFile = $fopen("output.txt","w");
+
+
+			begin
+				#100;
+				//write to external file
+				$fwrite(outputFile,"%b\n",o);
+			end
+
+			$fclose(outputFile);  
+
+			//$finish;
 		end
-
-		$fclose(outputFile);  
-
-		//$finish;
-  end
       
 endmodule
